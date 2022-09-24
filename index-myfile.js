1372c9349f037c7efde6e24b366357319ba16ca6
ShopskaSalad <akinkamilov@gmail.com>
2022-09-24 14:02:04 +0300

    my version / first commit

---
+++ index-myfile.js
@@ -0,0 +1,46 @@
+/**
+ * CHALLENGE 2
+ */
+
+// let x = 10;
+// const y = true;
+
+// const myObject = {
+//     a: x,
+//     b: y
+// };
+
+// console.log(myObject);
+
+// x=20;
+// let anotherObject;
+// anotherObject = {
+//     newA: x,
+//     b: y,
+//     c: myObject
+// }
+
+// console.log(anotherObject);
+
+/**
+ * CHALLENGE 3
+ */
+
+let myVariable;
+
+console.log(myVariable); 
+
+myVariable = 10;
+
+console.log(myVariable);
+
+myVariable = true;
+
+console.log(myVariable);
+
+myVariable = {
+    x: true,
+    y: 10
+};
+
+console.log(myVariable);
