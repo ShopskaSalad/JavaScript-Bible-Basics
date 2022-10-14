62775c1dde5b14a8d121746a1dc3ed72da032267
ShopskaSalad <akinkamilov@gmail.com>
2022-10-14 13:27:53 +0300

    my commits / added index-mytest.js

---
+++ index-mytest.js
@@ -0,0 +1,23 @@
+/**
+ * TEST1
+ */
+
+let myVariable1 = 10
+let myVariable2 = "5"
+
+console.log( +myVariable1 <= +myVariable2 );
+
+myVariable1 = "20"
+myVariable2 = 100
+
+console.log( +myVariable1 <= +myVariable2 );
+
+/**
+ * TEST2
+ */
+
+ let myNumber1 = 10
+ let myNumber2 = 3
+
+ console.log( myNumber1 % myNumber2 );
+ 
