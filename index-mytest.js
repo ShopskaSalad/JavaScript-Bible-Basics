db702966e8ab9eb2c053c9e9d7cef45786f41d6f
ShopskaSalad <akinkamilov@gmail.com>
2022-10-14 13:53:08 +0300

    my commits / added index-mytest.js

---
+++ index-mytest.js
@@ -0,0 +1,43 @@
+// /**
+//  * Task 1
+//  */
+
+// const b = 2;
+// let d = 15;
+
+// function myFn1(a){
+//     let b;
+//     let d = 10;
+//     myFn2(b);
+// }
+
+// function myFn2(a){
+//     let c = 5;
+//     console.log(a, b, c, d);
+// }
+
+// myFn1()
+
+// /**
+//  * Task 2
+//  */
+
+// "use strict"
+ 
+// function myFunction(){
+//      a = 2;
+//      return a;
+//  }
+
+//  myFunction();
+
+/**
+ * Task 3
+ * callback
+ */
+
+setTimeout( function myFn(){
+    console.log("test");
+}, 2000);
+
+myFn()
